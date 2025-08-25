ec74973278d3349b070f01a38e7470aa084c3172
Twinkle <twinkledevwanshi93@gmail.com>
2025-08-25 16:40:15 -0700

    fix jsx file

--- taxi-booking/src/App.jsx
+++ taxi-booking/src/App.jsx
@@ -1,27 +1,23 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Layout from "./Layout";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Booking from "./pages/Booking";
 
-function App(){
-  return(
-<Router>
-      {/* Page content */}
+function App() {
+  return (
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/booking" element={<Booking />} />    
-         </Routes>
-
-      {/* Footer shared on all pages */}
-      <footer className="footer">
-        <p>© 2025 Book_A_Taxi. All rights reserved.</p>
-      </footer>
-    </Router>
+        {/* Wrap Layout around all pages */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/booking" element={<Booking />} />
+        </Route>
+      </Routes>
   );
 }
 
